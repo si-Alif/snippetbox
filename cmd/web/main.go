902a82940ff6703a000edb4c8177dc888d44719a
si-Alif <shahrierislam9153@gmail.com>
2025-08-29 05:20:54 +0600

    ~home page configured

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,15 @@ import (
 func main(){
 
 	mux := http.NewServeMux()
+
+	//create a file server which will serve contents from the ./ui/static directory
+	file_server := http.FileServer(http.Dir("./ui/static/")) // this will a sub-tree path
+
+	// now we need to use the file_server server as the handler for serving file whenever there's a request to a endpoint with prefix /static/
+	mux.Handle("GET /static/" , http.StripPrefix("/static" , file_server))
+	// a request to /static/favicon.ico --> stripped /static --> result /favicon.ico went to file_server
+	// --> file_server looks up at ./ui/static/favicon.ico
+
 	mux.HandleFunc("GET /{$}" , home)
 	mux.HandleFunc("GET /snippet/view/{id}" , snippetView)
 	mux.HandleFunc("GET /snippet/create" , snippetCreate)
